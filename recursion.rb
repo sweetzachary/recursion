d69dbf0b9544546572523d297fb41e858cea0e8f
sweetzachary <a.ntigr16@gmail.com>
2021-12-20 16:38:12 +0200

    Add recursive implementation of Fibonacci Sequence algorithm.

--- recursion.rb
+++ recursion.rb
@@ -3,10 +3,18 @@ def fibs(n)
   a = 1
   b = 1
   n.times do |n|
-    arr.push b
+    arr.push a
     a, b = b, a+b
   end
   arr
 end
 
+def fibs_rec(n)
+  return Array.new(n, 1) if n <= 2
+  
+  prev = fibs_rec(n - 1)
+  prev + [prev[-1] + prev[-2]]
+end
+
 puts fibs 15
+puts fibs_rec 15
